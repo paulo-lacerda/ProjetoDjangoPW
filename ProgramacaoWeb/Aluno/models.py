485c4a2fffc5f02d6621e9e08ca1cfc82edab4de
pauloeoe <viniciu.skp@gmail.com>
2015-11-11 15:47:56 -0300

    2.0
    Remoção dos pacotes não utilizados, aluno, professor e discipliina.

--- ProgramacaoWeb/Aluno/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import Disciplina
-
-class Aluno(models.Model):
-    nome = models.CharField(max_length=100, null = False)
-    matricula = models.IntegerField(max_length=100, null = False)
-    email = models.CharField(max_length=100, null = False)
-    disciplina=models.ForeignKey(Disciplina.models.Disciplina)
-    professorDisciplina = models.ForeignKey()
--- ProgramacaoWeb/Professor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import Disciplina
-
-# Create your models here.
-
-class Professor(models):
-    nome = models.CharField(max_length=100, null = False)
-    disciplina = models.ForeignKey(Disciplina.models.Disciplina)
